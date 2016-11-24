4323c6791c8c46fe8555b4f63171a699ba88872e
Hieu Nguyen <hnnguyen45@gmail.com>
2016-11-24 11:36:05 -0800

    add checking password and endpoints

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
+
+var strategy = new BasicStrategy(function(username, password, callback) {
+    User.findOne({
+        username: username
+    }, function (err, user) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        if (!user) {
+            return callback(null, false, {
+                message: 'Incorrect username.'
+            });
+        }
+
+        user.validatePassword(password, function(err, isValid) {
+            if (err) {
+                return callback(err);
+            }
+
+            if (!isValid) {
+                return callback(null, false, {
+                    message: 'Incorrect password.'
+                });
+            }
+            return callback(null, user);
+        });
+    });
+});
+
+passport.use(strategy);
+
+
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
@@ -73,6 +110,16 @@ app.post('/users', jsonParser, function(req, res) {
         return res.status(201).json({});
     });
 });
+
+app.use(passport.initialize());
+
+
+app.get('/hidden', passport.authenticate('basic', {session: false}), function(req, res) {
+    res.json({
+        message: 'Luke... I am your father'
+    });
+});
+
 
 mongoose.connect('mongodb://localhost/auth').then(function() {
     app.listen(process.env.PORT || 8080);
